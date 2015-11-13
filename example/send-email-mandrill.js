b9fc45b055b6bba723e0bccba4c8a3523dd29785
David Guttman <david@davidguttman.com>
2015-11-13 10:31:27 -0800

    examples: added sendEmail mandrill example

---
+++ example/send-email-mandrill.js
@@ -0,0 +1,28 @@
+var powerdrill = require('powerdrill')('your-api-key-here')
+
+function sendEmail (emailOpts, cb) {
+  var message = powerdrill()
+
+  var to = emailOpts.email
+  var from = 'Authentic Accounts <email@domain.com>'
+
+  var subject, html
+  if (emailOpts.type === 'signup') {
+    subject = 'Confirm Your Account'
+    html = 'Please <a href="' + emailOpts.confirmUrl + '">confirm your account to continue</a>.'
+  } else if (emailOpts.type === 'change-password-request') {
+    subject = 'Password Reset'
+    html = 'If you would like to reset your password <a href="' + emailOpts.changeUrl + '">you may do so here</a>.'
+  } else {
+    return cb(new Error('Unknown email type'))
+  }
+
+  message
+    .to(to)
+    .from(from)
+    .subject(subject)
+    .html(html)
+    .trackClicks(false)
+
+  message.send(cb)
+}
